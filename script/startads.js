bca37e92160c91c4b075bd2e99d0b821adec5d86
Larkris <Larkris@users.noreply.github.com>
2023-06-02 01:23:58 +0800

    Add files via upload

--- script/startads.js
+++ script/startads.js
@@ -6,29 +6,6 @@ let body = $response.body;
 
 if (body) {
   switch (true) {
-    // 京东-开屏广告
-    case /^https:\/\/api\.m\.jd\.com\/client\.action\?functionId=start/.test(
-      url
-    ):
-      try {
-        let obj = JSON.parse(body);
-        for (let i = 0; i < obj.images.length; i++) {
-          for (let j = 0; j < obj.images[i].length; j++) {
-            if (obj.images[i][j].showTimes) {
-              obj.images[i][j].showTimes = 0;
-              obj.images[i][j].onlineTime = "2040-01-01 00:00:00";
-              obj.images[i][j].referralsTime = "2040-01-01 23:59:59";
-              obj.images[i][j].time = 0;
-            }
-          }
-        }
-        obj.countdown = 100;
-        obj.showTimesDaily = 0;
-        body = JSON.stringify(obj);
-      } catch (error) {
-        console.log(`京东-开屏广告, 出现异常: ` + error);
-      }
-      break;
     // 小爱音箱-开屏广告
     case /^https:\/\/hd\.mina\.mi\.com\/splashscreen\/alert/.test(url):
       try {
