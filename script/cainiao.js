58ed6ec7984226128958f0aa14c440aa0830c5c8
Larkris <Larkris@users.noreply.github.com>
2023-06-03 08:36:29 +0800

    Add files via upload

--- script/cainiao.js
+++ script/cainiao.js
@@ -1,7 +1,7 @@
 let okk = JSON.parse($response.body);
 switch (true) {
   case /cainiao\.guoguo\.nbnetflow\.ads\.mshow/.test($request.url):
-    const item = ["1316", "1332", "1275", "1308", "1340", "1382"];
+    const item = ["958", "1273", "1275", "1308", "1316", "1332", "1340", "1382", "1391"];
     for (let i of item) {
       if (okk.data?.[i]) {
         delete okk.data[i];
